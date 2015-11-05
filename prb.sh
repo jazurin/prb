a8e68fc4c1fa390279f9a97e90286681b4d7e017
op-jenkins <kibagami.jubei@gmail.com>
2015-11-05 12:58:26 -0600

    Update prb.sh

--- prb.sh
+++ prb.sh
@@ -2,10 +2,10 @@
 echo "Test 3"
 rm -rf file1 file2 prb.log
 # comment line below to cause an OP Build test failure
-touch file1
+#touch file1
 
 # change the string below to cause a OP HW test failure
-echo "GHPRB!" > file2
-#echo "foo" > file2
+#echo "GHPRB!" > file2
+echo "foo" > file2
 ls -al | tee prb.log
 
